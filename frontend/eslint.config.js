f07fdaae927aee349bf1e87bec7cf8c036b76694
kunal-singh <hi@thekunalsingh.com>
2026-04-05 17:45:34 +0530

    chore: added tailwind, shadcn, migrated to uv builds

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,7 @@
 import config from "@kunal-singh/eslint-config/react";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([{ ignores: ["dist", "*.config.js", ".lintstagedrc.js"] }, ...config]);
+export default defineConfig([
+  { ignores: ["**/dist/**", "eslint.config.js", "*.config.js", ".lintstagedrc.js"] },
+  ...config,
+]);
